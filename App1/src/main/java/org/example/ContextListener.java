60fb877aff563e43c6c88af958c9145af9950169
Ayusha200333 <ayushawijerathna@gmail.com>
2025-06-06 10:34:11 +0530

    add inbuildDBCP

--- App1/src/main/java/org/example/ContextListener.java
+++ App1/src/main/java/org/example/ContextListener.java
@@ -1,16 +1,43 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @WebListener
 public class ContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("Context initialized");
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/eventdb");
+        ds.setUsername("root");
+        ds.setPassword("Ijse@1234");
+        ds.setInitialSize(50);
+        ds.setMaxTotal(100);
+
+        ServletContext sc = sce.getServletContext();
+        sc.setAttribute("datasource", ds);
+
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
-        System.out.println("Context destroyed");
+        try {
+            ServletContext sc = sce.getServletContext();
+            BasicDataSource ds = (BasicDataSource) sc.getAttribute("datasource");
+            ds.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
