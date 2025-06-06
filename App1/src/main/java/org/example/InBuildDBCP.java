01f4311f6c9309280b3de992ceffdec2ebb6a750
Ayusha200333 <ayushawijerathna@gmail.com>
2025-06-06 10:58:59 +0530

    add to inbuildDBCP

--- App1/src/main/java/org/example/InBuildDBCP.java
+++ App1/src/main/java/org/example/InBuildDBCP.java
@@ -1,9 +1,50 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.Resource;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet("/inbuild")
 public class InBuildDBCP extends HttpServlet {
+    @Resource(name = "java:comp/env/jdbc/pool")
+    private DataSource ds;
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try{
+            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/eventdb","root","Ijse@1234");
+            ResultSet resultSet=connection.prepareStatement("select * from event").executeQuery();
+            List<Map<String,String>> elist=new ArrayList<>();
+            while(resultSet.next()){
+                Map<String,String> event=new HashMap<>();
+                event.put("eid",resultSet.getString("eid"));
+                event.put("ename",resultSet.getString("ename"));
+                event.put("edescription",resultSet.getString("edescription"));
+                event.put("edate",resultSet.getString("edate"));
+                event.put("eplace",resultSet.getString("eplace"));
+                elist.add(event);
+            }
+            resp.setContentType("application/json");
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(resp.getWriter(),elist);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
